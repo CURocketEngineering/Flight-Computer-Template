d8a0905baa443ea294da34f2c109380949f24ba2
Thomas Day <tkday05@gmail.com>
2026-03-12 17:32:08 -0400

    Added the correct pins for new sensors and updated old pins. Still need to decide the git repo to use for new sensors.

--- include/pins.h
+++ include/pins.h
@@ -5,17 +5,33 @@
 
 #ifdef PCB_MARTHA
 
-#define SENSOR_MISO PA6 
-#define SENSOR_MOSI PA7 
-#define SENSOR_SCK PA5
-#define SENSOR_BARO_CS PB2
-#define SENSOR_LSM_CS PB0 
-#define SENSOR_LIS_CS PA2
-#define FLASH_CS PB1
-#define DEBUG_LED PA9
+#define SENSOR_MISO_1 PA6 
+#define SENSOR_MOSI_1 PA7 
+#define SENSOR_SCK_1 PA5
+
+// Second SPI: used for sensors MS5607, BMI, and KX
+#define SENSOR_MISO_2 PC_11
+#define SENSOR_MOSI_2 PC_12
+#define SENSOR_SCK_2 PC_10
+
+// Sensors. MS5607, BMI, and KX are new!
+#define SENSOR_BARO_CS PB_14
+#define SENSOR_MS5607_CS PB_15
+#define SENSOR_BMI_G_CS PE0
+#define SENSOR_BMI_A_CS PE1 
+#define SENSOR_KX_CS PE2
+#define SENSOR_LSM_CS PE3
+#define SENSOR_LIS_CS PE4
+
+#define FLASH_CS PB5 // Note: this is W250 now
+
+#define LED_RED PE_12
+#define LED_GREEN PE_13
+#define LED_BLUE PE_14
+#define BUZZER_PIN PA1
 
 // For the Adafruit SPI Flash
-#define EXTERNAL_FLASH_USE_CS PB1
+#define EXTERNAL_FLASH_USE_CS PB5
 #define EXTERNAL_FLASH_USE_SPI SPI
 
 #endif // PCB_MARTHA
@@ -30,6 +46,7 @@
 #define SENSOR_LIS_CS PA3
 #define FLASH_CS PB1
 #define DEBUG_LED PA9
+#define ADC_VOLTAGE PA0
 
 #define EXTERNAL_FLASH_USE_CS PB1
 #define EXTERNAL_FLASH_USE_SPI SPI
